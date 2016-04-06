4952a2fbb58458c9555af130a917c5298733dede
jnf <jeremy.flores@gmail.com>
2016-04-06 13:31:37 -0700

    my solution to the exercise

--- Queue.rb
+++ Queue.rb
@@ -1,13 +1,22 @@
+#FIFO
 class Queue
   def initialize
+    @store = []
   end
   
   def dequeue
+    @store.pop
   end
   
   def enqueue(element)
+    @store.unshift element
   end
   
   def size
+    @store.length
+  end
+
+  def empty?
+    size == 0
   end
 end
--- job-simulation.rb
+++ job-simulation.rb
@@ -1,4 +1,47 @@
 require './Stack.rb'
 require './Queue.rb'
 
+class Company
+  MAX_EMPLOYEES = 6
 
+  attr_reader :waiting, :working
+
+  def initialize
+    @waiting = Queue.new
+    10.times { |i| wait! i+1 }
+
+    @working = Stack.new
+
+    end_of_the_month
+  end
+
+  def end_of_the_month
+    fire!
+    hire!
+  end
+
+  private
+
+  def hire!
+    until working.size >= MAX_EMPLOYEES
+      new_hire = waiting.dequeue
+      working.push new_hire
+    end
+  end
+
+  def fire!
+    return if working.empty?
+    number_to_fire.times do
+      fired_person = working.pop
+      wait! fired_person
+    end
+  end
+
+  def wait!(person)
+    waiting.enqueue person
+  end
+
+  def number_to_fire
+    rand(6) + 1
+  end
+end
